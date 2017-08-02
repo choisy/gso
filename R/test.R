4fac0440f0279f31e7bafba19a499291435507fa
Lucie Contamin <contamin.lucie@gmail.com>
2017-08-02 10:22:27 +0700

    test province names

--- R/test.R
+++ R/test.R
@@ -58,3 +58,37 @@ df %>% filter(province == "Binh Tri Tien", year == 2007, key == "Total") %>% .$v
   test %>% filter(province == "Quang Binh", year == 2007) %>% .["Total"] +
   test %>% filter(province == "Quang Tri", year == 2007) %>% .["Total"] +
   test %>% filter(province == "Thua Thien - Hue", year == 2007) %>% .["Total"]
+
+
+# test provinces names ---------------------------------------------------------
+
+prov92 <- c("An Giang", "Ba Ria - Vung Tau", "Bac Thai", "Ben Tre", "Binh Dinh",
+            "Binh Thuan", "Can Tho", "Cao Bang", "Dack Lak", "Dong Nai", "Dong Thap",
+            "Gia Lai", "Ha Bac", "Ha Giang", "Ha Noi", "Ha Tinh", "Hai Hung",
+            "Hai Phong", "Ho Chi Minh", "Hoa Binh", "Khanh Hoa",
+            "Kien Giang", "Kon Tum", "Lai Chau", "Lam Dong", "Lang Son", "Lao Cai",
+            "Long An",  "Minh Hai", "Nam Ha", "Nghe An", "Ninh Binh", "Ninh Thuan",
+            "Phu Yen", "Quang Binh", "Quang Nam - Da Nang", "Quang Ngai",
+            "Quang Ninh", "Quang Tri", "Soc Trang", "Son La", "Song Be", "Tay Ninh",
+            "Thai Binh", "Thanh Hoa", "Thua Thien - Hue", "Tien Giang", "Tra Vinh",
+            "Tuyen Quang", "Vinh Long", "Vinh Phu", "Yen Bai")
+
+prov04 <- c("An Giang", "Ba Ria - Vung Tau", "Bac Giang", "Bac Kan", "Bac Lieu",
+            "Bac Ninh", "Ben Tre",  "Binh Dinh", "Binh Duong", "Binh Phuoc",
+            "Binh Thuan", "Ca Mau", "Can Tho", "Cao Bang", "Da Nang", "Dak Lak",
+            "Dak Nong", "Dien Bien", "Dong Nai", "Dong Thap", "Gia Lai",
+            "Ha Giang", "Ha Nam", "Ha Noi", "Ha Tay", "Ha Tinh", "Hai Duong",
+            "Hai Phong", "Hau Giang", "Ho Chi Minh", "Hoa Binh", "Hung Yen",
+            "Khanh Hoa", "Kien Giang", "Kon Tum", "Lai Chau", "Lam Dong",
+            "Lang Son", "Lao Cai", "Long An", "Nam Dinh", "Nghe An",
+            "Ninh Binh", "Ninh Thuan", "Phu Tho", "Phu Yen", "Quang Binh",
+            "Quang Nam", "Quang Ngai", "Quang Ninh", "Quang Tri", "Soc Trang",
+            "Son La", "Tay Ninh", "Thai Binh", "Thai Nguyen", "Thanh Hoa",
+            "Thua Thien - Hue", "Tien Giang", "Tra Vinh", "Tuyen Quang",
+            "Vinh Long", "Vinh Phuc", "Yen Bai")
+
+testthat::expect_equal(
+  mean(df$province %in%
+         c("Dack Lak",prov92 %>% unique)), 1)
+
+
