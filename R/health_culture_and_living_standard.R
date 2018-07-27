495ae70123acfe5dbb83ab563eb9e41321483e03
Lucie Contamin <contamin.lucie@gmail.com>
2018-07-27 14:17:16 +0200

    Update package dictionary

--- R/health_culture_and_living_standard.R
+++ R/health_culture_and_living_standard.R
@@ -1,65 +1,67 @@
 #' Number of libraries under local management by province
-#'
+#' 
 #' Contains the data expressed : 2004-2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 704 rows and 4 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{
 #' number_of_books_in_library_thous_copies } A column of class : numeric. \item
 #' \code{ number_of_libraries_unit } A column of class : numeric. }
 #' @source General Statistical Office of Vietnam
-#'
-#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'culture_2'
 
 #' Number of editorial offices by province
-#'
+#' 
 #' Contains the data expressed : 2005-2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 504 rows and 3 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{
 #' number_of_editorial_offices } A column of class : integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'culture_3'
 
 #' Number of medical staff under provincial departments of health by province
-#'
+#' 
 #' Contains the data expressed : 2002-2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 896 rows and 6 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{ doctor } A column of
 #' class : integer. \item \code{ midwife } A column of class : integer. \item
 #' \code{ nurse } A column of class : integer. \item \code{ physician } A
 #' column of class : integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'health_11'
 
 #' Number of pharmaceutical staffs under provincial departments of health by
 #' province
-#'
+#' 
 #' Contains the data expressed : 2002-2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 896 rows and 5 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{
 #' assistant_pharmacists } A column of class : integer. \item \code{
 #' pharmacists_of_high_degree } A column of class : integer. \item \code{
 #' pharmacists_of_middle_degree } A column of class : integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'health_12'
 
 #' Number of people infected with HIV-AIDS and number of AIDS deaths by
 #' province
-#'
+#' 
 #' Contains the data expressed : 2011-2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 315 rows and 7 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{
@@ -69,15 +71,16 @@
 #' class : integer. \item \code{ hiv_infected_people_new_case_in } A column of
 #' class : integer. \item \code{ number_of_aids_deathsin } A column of class :
 #' integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'health_13'
 
 #' Number of health establishments under provincial departments of health by
 #' province
-#'
+#' 
 #' Contains the data expressed : 2005-2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 704 rows and 7 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{ hospital } A column
@@ -85,14 +88,15 @@
 #' integer. \item \code{ regional_polyclinic } A column of class : integer.
 #' \item \code{ sanatorium_and_rehabilitation_hospital } A column of class :
 #' integer. \item \code{ total } A column of class : integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'health_5'
 
 #' Number of patient beds under provincial departments of health by province
-#'
+#' 
 #' Contains the data expressed : 2002-2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 896 rows and 7 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{ hospital } A column
@@ -100,39 +104,42 @@
 #' integer. \item \code{ regional_polyclinic } A column of class : integer.
 #' \item \code{ sanatorium_and_rehabilitation_hospital } A column of class :
 #' integer. \item \code{ total } A column of class : integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'health_8'
 
 #' Poverty rate by province
-#'
+#' 
 #' Contains the data expressed : 2006-2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 448 rows and 3 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{ poverty_rate } A
 #' column of class : numeric. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'living_standard_16'
 
 #' Percentage of household using electricity by province
-#'
+#' 
 #' Contains the data expressed : 2002-2014, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 448 rows and 3 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{
 #' percentage_of_household_using_electricity } A column of class : numeric. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'living_standard_23'
 
 #' Percentage of households in 2014 having house by type of house and by
 #' province
-#'
+#' 
 #' Contains the data expressed : 2014, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 63 rows and 6 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ less_temporary_house } A column of class : numeric. \item \code{
@@ -140,14 +147,15 @@
 #' semi_permanent_house } A column of class : numeric. \item \code{
 #' simple_permanent_house } A column of class : numeric. \item \code{ total } A
 #' column of class : numeric. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'living_standard_26'
 
 #' Average dwelling area per capita in 2014 by type of house and by province
-#'
+#' 
 #' Contains the data expressed : 2014, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 63 rows and 6 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ less_temporary_house } A column of class : numeric. \item \code{
@@ -155,15 +163,16 @@
 #' semi_permanent_house } A column of class : numeric. \item \code{
 #' simple_permanent_house } A column of class : numeric. \item \code{ total } A
 #' column of class : numeric. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'living_standard_28'
 
 #' Monthly average income per capita at current prices by income source and by
 #' province
-#'
+#' 
 #' Contains the data expressed : 2010-2014, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 189 rows and 7 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{
@@ -171,29 +180,31 @@
 #' non_agriculture_forestry_fishery } A column of class : integer. \item \code{
 #' others } A column of class : integer. \item \code{ salary_wage } A column of
 #' class : integer. \item \code{ total } A column of class : integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'living_standard_3'
 
 #' Average collected solid waste treated per day in 2015 by province
-#'
+#' 
 #' Contains the data expressed : 2015, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 63 rows and 3 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ total_of_collected_solid_waste } A column of class : integer. \item
 #' \code{
 #' total_of_collected_solid_waste_treated_in_accordance_with_corresponding_defined_national_criteria
 #' } A column of class : integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'living_standard_33'
 
 #' Monthly average income per capita at current prices by income quintile and
 #' by province
-#'
+#' 
 #' Contains the data expressed : 2010-2014, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 189 rows and 8 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{ general_average } A
@@ -201,15 +212,16 @@
 #' integer. \item \code{ quintile_2 } A column of class : integer. \item \code{
 #' quintile_3 } A column of class : integer. \item \code{ quintile_4 } A column
 #' of class : integer. \item \code{ quintile_5 } A column of class : integer. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'living_standard_5'
 
 #' Difference between the highest income quintile and the lowest income
 #' quintile on monthly average income per capita at current prices by province
-#'
+#' 
 #' Contains the data expressed : 2010-2014, by year and by province.
-#'
-#'
+#' 
+#' 
 #' @format An object of class \code{data.frame} with 189 rows and 5 columns.
 #' \itemize{ \item \code{ province } A column of class : character. \item
 #' \code{ year } A column of class : integer. \item \code{
@@ -219,6 +231,7 @@
 #' column of class : numeric. \item \code{
 #' the_highest_income_quintile_compared_with_the_lowest_income_quintile_time }
 #' A column of class : numeric. }
-#' @source General Statistical Office of Vietnam (\url{http://gso.gov.vn/Default_en.aspx?tabid=491}).
+#' @source General Statistical Office of Vietnam
+#' (\url{http://gso.gov.vn/Default_en.aspx?tabid=491})
 'living_standard_7'
 
