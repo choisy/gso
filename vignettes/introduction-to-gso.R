71ebfd2a20323f95cd82a62ce5c7482f6e317672
Lucie Contamin <contamin.lucie@gmail.com>
2018-09-10 18:08:35 +0700

    Update vignette usage

--- vignettes/introduction-to-gso.R
+++ vignettes/introduction-to-gso.R
@@ -3,47 +3,30 @@
 
 ## ------------------------------------------------------------------------
 library(gso)
-
-## ----eval=F--------------------------------------------------------------
-#  write.table(gso::data_frame_summary, "summary.csv", quote = F, sep = ",", row.names = F)
-#  write.table(gso::agriculture_1, "agriculture.csv", quote = F, sep = ",", row.names = F)
+library(dplyr) # for data manipulation
 
 ## ------------------------------------------------------------------------
-head(data_frame_summary)
-str(data_frame_summary)
+head(content)
 
 ## ----eval=F--------------------------------------------------------------
-#  ?data_frame_summary
+#  ?content
 
 ## ---- message=FALSE------------------------------------------------------
 #install.packages("dplyr")
+#install.packages("tidyr")
 library(dplyr)
+library(tidyr)
+glimpse(content)
 
 ## ------------------------------------------------------------------------
-avail_data <- filter(data_frame_summary, priority == 1)
-dim(avail_data)
-head(avail_data)
+agri_forest_fish <- filter(content, category == "Agriculture, Forestry and Fishery")
+glimpse(agri_forest_fish)
 
 ## ------------------------------------------------------------------------
-unique(avail_data$category) # gives you the list of category available
-agri_forest_fish <- filter(data_frame_summary, category == "Agriculture, Forestry and Fishery")
-str(agri_forest_fish)
+sel <- grep("hiv", content$data_frame, ignore.case = TRUE)
+content[sel, ] # permit to print more information
+df_hiv <- unnest(content[sel, "data"])
 
 ## ---- eval = F-----------------------------------------------------------
-#  View(avail_data)
-#  # Call the data frame
-#  agriculture_1
-
-## ------------------------------------------------------------------------
-head(pop_size)
-str(pop_size)
-
-## ------------------------------------------------------------------------
-?pop_size
-
-## ------------------------------------------------------------------------
-head(agriculture_1)
-
-## ------------------------------------------------------------------------
-str(agriculture_1)
+#  View(content)
 
