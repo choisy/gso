1396713afb4fbce3c48c9f61ffb1125fab94c6bb
Lucie Contamin <contamin.lucie@gmail.com>
2019-01-09 15:47:10 +0700

    Add error message in extract_df and update documentation

--- R/extract_df.R
+++ R/extract_df.R
@@ -1,8 +1,10 @@
 #' Extract data frame from content
 #'
-#' Extracts a data frame or a list of data frame in the content data frame.
+#' Extracts a data frame or a list of data frame from the name `data_name` of
+#' the data frame selected in the content data frame.
 #'
-#' @param keyword a vector of character string
+#' @param keyword a vector of character string, of value from the column
+#'     `data_name` of the content data frame
 #'
 #' @return a data frame or a list of data frame
 #'
@@ -17,6 +19,13 @@
 #' # for multiple keyword
 #' lst_demo <- extract_df(c("demography_5", "demography_3"))
 extract_df <- function(keyword) {
+
+  if(all(keyword %in% content$data_name) == FALSE) {
+    stop(
+      paste0("The argument `keyword` should contain character vector of value,",
+             ", contained in the 'data_name' column of 'content' data frame."))
+  }
+
   lst <- lapply(keyword, function(x) {
     df <- content %>%
       filter(data_name == x) %>%
