9bc77ea0633aceb6e62b49a37b000bd934684898
Lucie Contamin <contamin.lucie@gmail.com>
2019-04-17 17:34:05 +0200

    remove dplyr, purrr and magrittr dependencies

--- R/extract_df.R
+++ R/extract_df.R
@@ -8,8 +8,6 @@
 #'
 #' @return a data frame or a list of data frame
 #'
-#' @importFrom magrittr %>%
-#' @importFrom dplyr filter select
 #' @export
 #'
 #' @examples
@@ -27,10 +25,8 @@ extract_df <- function(keyword) {
   }
 
   lst <- lapply(keyword, function(x) {
-    df <- content %>%
-      filter(data_name == x) %>%
-      select(data) %>%
-      unlist(FALSE)
+    df <- subset(content, data_name == x, data)
+    df <- unlist(df, FALSE)
   })
   if (length(lst) == 1) {lst <- lst[[1]][[1]]}
   lst
